a832a37bf5b8ea81b92d50c5a6087913cdd66fd4
daltschu <daltschu@192.168.2.100>
2014-10-05 16:48:07 +0200

    delete orientation lock

--- files/main.js
+++ files/main.js
@@ -7,17 +7,12 @@ var ctx, //context for drawing in canvas
     deviceWidth, //device avaialable max width
     deviceHeight; //device available max height
 
-function onOrientationchange()
-{
-    var isPortrait = window.orientation % 180 === 0;
-};
-
 function setup()
 {
     ctx = document.getElementById('mycanvas').getContext('2d');
     deviceWidth = window.screen.availWidth;
     deviceHeight = window.screen.availHeight;
-    window.screen.lockOrientation('portrait');
+    
 }
 
 
